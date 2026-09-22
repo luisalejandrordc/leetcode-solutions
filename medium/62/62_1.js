b18c20216677f25ded2886320634ff372c7e3ed9
luisalejandrordc <luisalejandrordc@gmail.com>
2026-09-22 18:29:24 -0500

    add medium/62/62_2.js

--- medium/62/62_1.js
+++ medium/62/62_1.js
@@ -36,3 +36,17 @@ var uniquePaths = function (m, n) {
 console.log(uniquePaths(3, 7));
 console.log(uniquePaths(3, 2));
 console.log(uniquePaths(100, 100));
+
+/**
+ * @param {Function} fn
+ * */
+function timedCall(fn, ...args) {
+  const start = performance.now();
+  const result = fn(...args);
+  const end = performance.now();
+  console.log(`Execution time: ${end - start} ms`);
+  return result;
+}
+
+const foo = timedCall(uniquePaths, 100, 100);
+console.log(foo);
--- medium/62/62_2.js
+++ medium/62/62_2.js
@@ -1,38 +1,35 @@
-const DIRECTIONS = [
-  [0, 1],
-  [1, 0],
-];
-
-let count;
-
-/**
- * @param {number} m
- * @param {number} n
- * @param {number} x
- * @param {number} y
- * @param {number[][]} memo
- * */
-function unique(m, n, x, y, memo) {
-  if (memo[x][y] === undefined) {
-    let count = 0;
-    for (dir of DIRECTIONS) count += unique(m, n, x + dir[0], y + dir[1], memo);
-    memo[x][y] = count;
-  }
-  return memo[x][y];
-}
-
 /**
  * @param {number} m
  * @param {number} n
  * @return {number}
  * */
 var uniquePaths = function (m, n) {
-  const memo = Array.from({ length: m }, () => new Array(n));
-  for (let c = 0; c < n; c++) memo[m - 1][c] = 1;
-  for (let r = 0; r < m; r++) memo[r][n - 1] = 1;
-  return unique(m, n, 0, 0, memo);
+  const dp = Array.from({ length: m }, () => new Array(n));
+  for (let c = 0; c < n; c++) dp[m - 1][c] = 1;
+  for (let r = 0; r < m; r++) dp[r][n - 1] = 1;
+
+  for (let r = m - 2; r >= 0; r--)
+    for (let c = n - 2; c >= 0; c--) {
+      dp[r][c] = dp[r + 1][c] + dp[r][c + 1];
+    }
+
+  return dp[0][0];
 };
 
 console.log(uniquePaths(3, 7));
 console.log(uniquePaths(3, 2));
 console.log(uniquePaths(100, 100));
+
+/**
+ * @param {Function} fn
+ * */
+function timedCall(fn, ...args) {
+  const start = performance.now();
+  const result = fn(...args);
+  const end = performance.now();
+  console.log(`Execution time: ${end - start} ms`);
+  return result;
+}
+
+const foo = timedCall(uniquePaths, 100, 100);
+console.log(foo);
